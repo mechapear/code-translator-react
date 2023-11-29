00e302a9c81fb786b606e6666e9ce9c3b5d57ba4
mechapear <mechapear@gmail.com>
2023-11-29 10:12:15 +0700

    feat: add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 const MORSE_CODE: Record<string, string> = {
   A: '.-',
@@ -64,14 +64,28 @@ export default function App() {
   const decodedText = decodeMorse(input)
   const resultText = isCaesar ? decodeCaesar(decodedText) : decodedText
 
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+  function handleInputChange(event: ChangeEvent<HTMLTextAreaElement>) {
     setInput(event.target.value)
   }
 
   return (
     <>
-      <div>
-        <label className="relative mb-5 inline-flex cursor-pointer items-center">
+      <div className="absolute left-1/2 top-1/2 min-w-[320px] -translate-x-1/2 -translate-y-1/2 md:min-w-[400px]">
+        <label
+          htmlFor="message"
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your message
+        </label>
+        <textarea
+          id="message"
+          rows={4}
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          placeholder="Write your message here..."
+          onChange={handleInputChange}
+        />
+
+        <label className="relative mb-6 mt-2 inline-flex cursor-pointer items-center">
           <input
             className="peer sr-only"
             type="checkbox"
@@ -83,23 +97,20 @@ export default function App() {
             Caesar Cipher
           </span>
         </label>
-        <label>
-          Code
-          <input
-            type="textarea"
-            className="border"
-            onChange={handleInputChange}
-          />
+
+        <label
+          htmlFor="message"
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Result message
         </label>
-        <label>
-          Text
-          <input
-            type="textarea"
-            className="border"
-            value={resultText}
-            readOnly
-          />
-        </label>
+        <textarea
+          id="message"
+          rows={4}
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+          value={resultText}
+          disabled
+        />
       </div>
     </>
   )
